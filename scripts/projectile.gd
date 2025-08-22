4d74d912e0f14e431fb13bea3866f3fbc9d98f54
Tanner <tannerelephant@gmail.com>
2025-08-22 16:26:04 -0700

    stop player from being able to shoot payload

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -8,7 +8,7 @@ func _process(delta: float) -> void:
 	global_position += direction * speed * delta
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if (body.has_node("Health")):
+	if body.has_node("Health") and body is Enemy:
 		var health = body.get_node("Health") as Health
 		health.take_damage(damage)
-		queue_free()
+	queue_free()
