438413c6760d84aadf025bee33b0275f20dcd373
Tanner <tannerelephant@gmail.com>
2025-08-22 15:56:44 -0700

    add basic enemy spawner

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,6 +3,9 @@ class_name Enemy extends Node2D
 @export var target: Node2D
 var speed := 200
 
+func _init() -> void:
+	target = Payload
+
 func _process(delta: float) -> void:
 	if target != null:
 		var direction = (target.global_position - global_position).normalized()
---
+++ scripts/enemy_spawner.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@onready var timer: Timer = $Timer
+@export var enemy_prefab: PackedScene
+@export var spawnpoints: Array[Node2D]
+var spawn_interval = 4
+
+func _on_timer_timeout() -> void:
+	spawn_enemy()
+	timer.start(spawn_interval)
+
+func spawn_enemy():
+	var spawnpoint = spawnpoints.pick_random()
+	var enemy = enemy_prefab.instantiate() as Enemy
+	get_tree().root.add_child(enemy)
+	enemy.global_position = spawnpoint.global_position
