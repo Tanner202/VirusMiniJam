2f6d2c62a213225770b53d1fa0b20a208beb68c2
Tanner <tannerelephant@gmail.com>
2025-08-22 16:13:41 -0700

    convert player movement to velocity based

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var projectile_prefab: PackedScene
 @export var shooting_point: Node2D
@@ -6,16 +6,26 @@ extends Node2D
 var speed := 400
 
 func _process(delta: float) -> void:
-	if (Input.is_action_pressed("up")):
-		global_position.y -= speed * delta
-	if (Input.is_action_pressed("down")):
-		global_position.y += speed * delta
-	if (Input.is_action_pressed("right")):
-		global_position.x += speed * delta
-	if (Input.is_action_pressed("left")):
-		global_position.x -= speed * delta
+	var direction = Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		direction.y = -1
+	if Input.is_action_pressed("down"):
+		direction.y = 1
+	if Input.is_action_pressed("up") and Input.is_action_pressed("down"):
+		direction.y = 0
+		
+	if Input.is_action_pressed("left"):
+		direction.x = -1
+	if Input.is_action_pressed("right"):
+		direction.x = 1
+	if Input.is_action_pressed("right") and Input.is_action_pressed("left"):
+		direction.x = 0
+		
 	if (Input.is_action_just_pressed("left_click")):
 		shoot()
+	
+	velocity = direction * speed
+	move_and_slide()
 
 func shoot():
 	var mouse_position = get_global_mouse_position()
