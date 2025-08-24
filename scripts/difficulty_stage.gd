8b8ad2402f41fd80cf7a54fb22d1685404711098
Tanner <tannerelephant@gmail.com>
2025-08-23 19:41:56 -0700

    add difficulty stages

---
+++ scripts/difficulty_stage.gd
@@ -0,0 +1,8 @@
+class_name DifficultyStage
+
+var end_time
+var enemy_spawn_speed
+
+func _init(end_time: float, enemy_spawn_speed: float) -> void:
+	self.end_time = end_time
+	self.enemy_spawn_speed = enemy_spawn_speed
--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -4,7 +4,21 @@ extends Node
 @export var enemy_prefab: PackedScene
 @export var spawnpoints: Array[Node2D]
 @export var enemy_target: Node2D
+var time_elapsed: float
+var difficulty_stages: Array[DifficultyStage] = [
+	DifficultyStage.new(5, 3),
+	DifficultyStage.new(10, 2.5),
+	DifficultyStage.new(20, 1.9),
+	DifficultyStage.new(30, 1.75),
+	DifficultyStage.new(35, 1.5)
+]
 var spawn_interval = 4
+
+func _process(delta: float) -> void:
+	time_elapsed += delta
+	for stage in difficulty_stages:
+		if stage.end_time < time_elapsed:
+			spawn_interval = stage.enemy_spawn_speed
 
 func _on_timer_timeout() -> void:
 	spawn_enemy()
