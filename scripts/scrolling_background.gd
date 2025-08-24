de80175f5c3bfc9d3dd47b69df184795dc233b26
Tanner <tannerelephant@gmail.com>
2025-08-24 12:52:10 -0700

    fix scrolling background

--- scripts/scrolling_background.gd
+++ scripts/scrolling_background.gd
@@ -1,12 +1,6 @@
-extends Sprite2D
+extends ParallaxLayer
 
-var og_position
 var speed = 200
 
-func _ready() -> void:
-	og_position = global_position
-
 func _process(delta: float) -> void:
-	global_position.y += speed * delta
-	if global_position.y > 5000:
-		global_position = og_position
+	motion_offset.y += speed * delta
